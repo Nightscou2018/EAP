a9b8422e6e977f48b915a9681ae48150b50ecf79
Elviro <j.pereirajunior@student.fontys.nl>
2018-04-21 17:53:50 +0200

    added test with logs in seconds, but I presume they are not relevant since we care about the minutes and hours

--- test/test_basal.cpp
+++ test/test_basal.cpp
@@ -8,15 +8,15 @@ using namespace LIB_PROFILE_BASAL_BASAL;
 
 TEST(TestBasalLookUp, NullSchedule) {
   Helper helper;
-  time_t now = time(0);
+  time_t givenTime = time(0);
   int32_t result;
-  result = BasalLookup(helper.GetSchedule(), now);
+  result = BasalLookup(helper.GetSchedule(), givenTime);
 
   EXPECT_EQ(result, -1);
 }
 
 
-TEST(TestBasalLookUp, NullTime) {
+TEST(TestBasalLookUp, NullGivenTime) {
   Helper helper;
 
   const char * start  = "01:00:00";
@@ -24,14 +24,14 @@ TEST(TestBasalLookUp, NullTime) {
 
   time_t startMin  = helper.StringToTime(start);
   time_t start2Min = helper.StringToTime(start1);
-  time_t getAtTime = 0;
+  time_t givenTime;
 
   int32_t result;
 
   helper.AddToSchedule(start, 5, helper.Minutes(startMin));
   helper.AddToSchedule(start1, 2, helper.Minutes(start2Min));
 
-  result = BasalLookup(helper.GetSchedule(), getAtTime);
+  result = BasalLookup(helper.GetSchedule(), givenTime);
 
   EXPECT_EQ(result, 5);
 }
@@ -42,60 +42,84 @@ TEST(TestBasalLookUp, BadSchedule) {
   const char * start  = "10:20:00";
   const char * nowStr = "10:45:00";
 
-  time_t now      = helper.StringToTime(nowStr);
-  time_t startMin = helper.StringToTime(start);
+  time_t givenTime = helper.StringToTime(nowStr);
+  time_t startMin  = helper.StringToTime(start);
 
   int32_t result;
 
   helper.AddToSchedule(start, 0.0, helper.Minutes(startMin));
 
-  result = BasalLookup(helper.GetSchedule(), now);
+  result = BasalLookup(helper.GetSchedule(), givenTime);
 
   EXPECT_EQ(result, -1);
 }
 
-TEST(TestBasalLookUp, BasalNow) {
+TEST(TestBasalLookUp, BasalGivenTime) {
   Helper helper;
 
   const char * start  = "10:00:00";
   const char * start1 = "10:20:00";
   const char * start2 = "10:25:00";
   const char * start3 = "10:30:00";
-  const char * nowStr = "10:30:00";
+  const char * nowStr = "10:29:00";
 
   time_t startMin  = helper.StringToTime(start);
   time_t start1Min = helper.StringToTime(start1);
   time_t start2Min = helper.StringToTime(start2);
   time_t start3Min = helper.StringToTime(start3);
-  time_t now       = helper.StringToTime(nowStr);
+  time_t givenTime = helper.StringToTime(nowStr);
 
   helper.AddToSchedule(start , 5, helper.Minutes(startMin));
   helper.AddToSchedule(start1 , 2, helper.Minutes(start1Min));
   helper.AddToSchedule(start2 , 15, helper.Minutes(start2Min));
   helper.AddToSchedule(start3 , 1, helper.Minutes(start3Min));
 
-  int32_t result = BasalLookup(helper.GetSchedule(), now);
+  int32_t result = BasalLookup(helper.GetSchedule(), givenTime);
 
   EXPECT_EQ(result, 15);
 }
 
-TEST(TestBasalLookUp, minutesBiggerThanLastLog) {
+TEST(TestBasalLookUp, MinutesBiggerThanLastLog) {
   Helper helper;
 
   const char * start  = "10:00:00";
   const char * start1 = "10:20:00";
   const char * nowStr = "10:21:00";
 
-  time_t now       = helper.StringToTime(nowStr);
+  time_t givenTime = helper.StringToTime(nowStr);
   time_t startMin  = helper.StringToTime(start);
   time_t start1Min = helper.StringToTime(start1);
 
   helper.AddToSchedule(start , 5, helper.Minutes(startMin));
   helper.AddToSchedule(start1 , 2, helper.Minutes(start1Min));
 
-  int32_t result = BasalLookup(helper.GetSchedule(), now);
+  int32_t result = BasalLookup(helper.GetSchedule(), givenTime);
 
   EXPECT_EQ(result, -1);
+}
+
+TEST(TestBasalLookUp, TimeLogsWithSeconds) {
+  Helper helper;
+
+  const char * start  = "10:01:10";
+  const char * start1 = "10:02:20";
+  const char * start2 = "10:03:30";
+  const char * start3 = "10:04:31";
+  const char * nowStr = "10:01:20";
+
+  time_t startMin  = helper.StringToTime(start);
+  time_t start1Min = helper.StringToTime(start1);
+  time_t start2Min = helper.StringToTime(start2);
+  time_t start3Min = helper.StringToTime(start3);
+  time_t givenTime = helper.StringToTime(nowStr);
+
+  helper.AddToSchedule(start , 5, helper.Minutes(startMin));
+  helper.AddToSchedule(start1 , 2, helper.Minutes(start1Min));
+  helper.AddToSchedule(start2 , 15, helper.Minutes(start2Min));
+  helper.AddToSchedule(start3 , 1, helper.Minutes(start3Min));
+
+  int32_t result = BasalLookup(helper.GetSchedule(), givenTime);
+  EXPECT_EQ(result, 5);
 }
 
 TEST(TestMaxDailyBasal, emptyInput) {
@@ -105,7 +129,7 @@ TEST(TestMaxDailyBasal, emptyInput) {
   EXPECT_EQ(result, -1);
 }
 
-TEST(TestMaxDailyBasal, correctInput) {
+TEST(TestMaxDailyBasal, CorrectInput) {
   Helper helper;
 
   const char * start  = "10:00:00";
