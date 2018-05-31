7d7ea1970857af0d974fad7c343538c3205fc899
Elviro <j.pereirajunior@student.fontys.nl>
2018-05-31 14:28:06 +0200

    fixed crc test file

--- lib/communication/raw/validation/crc.h
+++ lib/communication/raw/validation/crc.h
@@ -3,16 +3,19 @@
 #define LIB_COMMUNICATION_RAW_VALIDATION_CRC_H_
 
 #include <inttypes.h>
-
+namespace communication {
+namespace raw {
 namespace validation {
 class CRC {
  public:
-    static uint8_t Reflect(const uint8_t input);
-    static uint8_t CRC8WCDMA(const uint8_t data[], unsigned int count);
+      static uint8_t Reflect(const uint8_t input);
+      static uint8_t CRC8WCDMA(const uint8_t data[], unsigned int count);
  private:
-    static const uint8_t crcTable[];
-    CRC() {}
+      static const uint8_t crcTable[];
+      CRC() {}
 };
 
 }  // namespace validation
+}  // namespace raw
+}  // namespace communication
 #endif  // LIB_COMMUNICATION_RAW_VALIDATION_CRC_H_
